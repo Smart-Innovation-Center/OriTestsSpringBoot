d0f6b9cbaf0a8170df1c3432e0ec777c1b7d8bc7
Ori Hénoc <47244708+OriHenoc@users.noreply.github.com>
2022-07-11 10:05:38 +0000

    Delete src directory

--- src/main/java/com/orihenoc/crudetudiants/service/EtudiantService.java
+++
@@ -1,8 +0,0 @@
-package com.orihenoc.crudetudiants.service;
-
-import java.util.List;
-import com.orihenoc.crudetudiants.entity.Etudiant;
-
-public interface EtudiantService {
-	List<Etudiant> getAllEtudiants();
-}
